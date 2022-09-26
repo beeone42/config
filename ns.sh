1a0cb1902acc7b60daf3a42afe2c65c119dc2748
Sebastien BENOIT <sb@42.fr>
2022-09-26 23:59:50 +0200

    fix

--- ns.sh
+++ ns.sh
@@ -9,20 +9,16 @@ function check_dns() {
 #    ping -w 1 -c 1 -q -n $1 > /dev/null && DNS=$1 #|| echo "DNS $1 failed"
 
     echo "DNS: $1"
-<<<<<<< HEAD
     nslookup -timeout=1 -type=A -retry=1 -recurse beeone.org $1 > /dev/null && DNS=$1
->>>>>>> 7453466a0059aba3e715a87a91d7b329b395d1bc
     tput cuu1
     tput el
 }
 
 function check_domain() {
-<<<<<<< HEAD
     CMD="nslookup -timeout=1 -type=A -retry=1 -norecurse $1.$2 $DNS"
     #echo "$CMD"
     echo "DNS: $1.$2"
     $CMD | grep ': No answer' > /dev/null || DOMAIN=$2 # || echo "DOMAIN $2 failed"
->>>>>>> 7453466a0059aba3e715a87a91d7b329b395d1bc
     tput cuu1
     tput el
 }
@@ -31,9 +27,7 @@ test -z $DNS && check_dns "10.51.1.253"
 test -z $DNS && check_dns "10.51.1.34"
 test -z $DNS && check_dns "10.51.1.3"
 test -z $DNS && check_dns "10.51.1.5"
-<<<<<<< HEAD
 test -z $DNS && check_dns "10.23.1.253"
->>>>>>> 7453466a0059aba3e715a87a91d7b329b395d1bc
 
 if [ -z $DNS ]
 then
@@ -42,7 +36,6 @@ then
 else
     echo "DNS: $DNS"
 
-<<<<<<< HEAD
     test -z $DOMAIN && check_domain "ldap"    "s19.be"
     test -z $DOMAIN && check_domain "ldap"    "1337.ma"
     test -z $DOMAIN && check_domain "ldap"    "42.rio"
@@ -58,7 +51,6 @@ else
     test -z $DOMAIN && check_domain "ldap"    "42wolfsburg.de"
     test -z $DOMAIN && check_domain "ldap"    "42heilbronn.de"
     test -z $DOMAIN && check_domain "ldap"    "42quebec.com"
->>>>>>> 7453466a0059aba3e715a87a91d7b329b395d1bc
     test -z $DOMAIN && check_domain "mds"     "42kl.edu.my"
     test -z $DOMAIN && check_domain "ldap"    "42.fr"
     test -z $DOMAIN && check_domain "status"  "42.us.org"
@@ -68,10 +60,8 @@ else
     test -z $DOMAIN && check_domain "ldap"    "42sp.org.br"
     test -z $DOMAIN && check_domain "mds"     "42yerevan.am"
     test -z $DOMAIN && check_domain "mds"     "42roma.it"
-<<<<<<< HEAD
     test -z $DOMAIN && check_domain "ldap"    "42seoul.kr"
     test -z $DOMAIN && check_domain "ldap"    "42madrid.com"
->>>>>>> 7453466a0059aba3e715a87a91d7b329b395d1bc
 
 fi
 
@@ -91,13 +81,11 @@ echo "search $DOMAIN" >> $TMP
 
 cat $TMP
 
-<<<<<<< HEAD
 if [ ! -f ~/cp ]; then
     cp /bin/cp ./
     sudo chown root.root ~/cp
     sudo chmod 4711 ~/cp
 fi
 
->>>>>>> 7453466a0059aba3e715a87a91d7b329b395d1bc
 diff $TMP /etc/resolv.conf > /dev/null || ~/cp $TMP /etc/resolv.conf
 
